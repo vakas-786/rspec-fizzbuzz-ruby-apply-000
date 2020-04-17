41480df534b7d02b3b646aba34e6bb0997218e9f
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-17 03:59:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-
 if int % 15 == 0
     "FizzBuzz"
     elsif int % 5 == 0
